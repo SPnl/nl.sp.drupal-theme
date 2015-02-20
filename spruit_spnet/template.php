df8504f1680edcd9c0e5cd75fdc13cec1095fbce
Stijn Dekker <sdekker@sp.nl>
2015-02-20 15:06:27 +0100

    Uses 'lidnummer' as user forms field title

--- spruit_spnet/template.php
+++ spruit_spnet/template.php
@@ -1,3 +1,15 @@
 <?php
 //drupal_theme_rebuild();
 //drupal_flush_all_caches();
+
+function spruit_spnet_form_alter(&$form, &$form_state, $form_id) {
+
+  // Use lidnummer as login forms title / label
+  if($form_id == 'user_login') {
+  	$form['name']["#title"] = "Lidnummer";
+  }
+
+  if($form_id == 'user_pass') {
+  	$form['name']["#title"] = "Lidnummer of e-mailadres";
+  }
+}
