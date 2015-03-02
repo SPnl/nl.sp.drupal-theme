eb01957daa924e0d4eaeb28c0442e34e55d20f88
Kevin Levie <kevin@levity.nl>
2015-03-02 14:12:18 +0100

    Fix emailverzending

--- spruit_spnet/template.php
+++ spruit_spnet/template.php
@@ -1,3 +1,15 @@
 <?php
 //drupal_theme_rebuild();
 //drupal_flush_all_caches();
+
+function spruit_spnet_form_alter(&$form, &$form_state, $form_id) {
+
+  // Replace username with 'lidnummer' on forms title / label
+  if($form_id == 'user_login') {
+  	$form['name']["#title"] = "Lidnummer";
+  }
+
+  if($form_id == 'user_pass') {
+  	$form['name']["#title"] = "Lidnummer of e-mailadres";
+  }
+}
